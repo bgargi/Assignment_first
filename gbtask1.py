b5b287e0187f752045dd73362535273efa77619d
Gargi Balasubramaniam <gargi.solaria@gmail.com>
2016-09-12 17:46:05 +0530

    first

---
+++ gbtask1.py
@@ -0,0 +1,17 @@
+c='Orange'
+print c[:1:]
+print c[:2:]
+print c[:3:]
+print c[:4:]
+print c[:5:]
+print c[:6:]
+print c[::-1]
+print c[:0:-1]
+print c[:1:-1]
+print c[:2:-1]
+print c[:3:-1]
+print c[:4:-1]
+print c[:5:-1]
+print c[:6:-1]
+
+
---
+++ gbtask2.py
@@ -0,0 +1,4 @@
+mass=input('Please enter the mass in kg.')
+acc=input('Please enter the acceleration in m/s^2')
+force=mass*acc
+print "The value of force in newton is",force
